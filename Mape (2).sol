60408e5c07a0ab6fe0c87982dce5480d51072aa7
MetaAngelDAO <96368688+MetaAngelDAO@users.noreply.github.com>
2022-03-01 21:40:09 +1100

    Update Mape (2).sol

--- Mape (2).sol
+++ Mape (2).sol
@@ -3,7 +3,7 @@
 
 pragma solidity ^0.8.4;
 
-import "./ERC721A.sol";
+import "erc721a/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 
